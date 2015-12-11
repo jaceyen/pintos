2728d8cad98ad19ea479998c063b753525f825b4
jaceyen <meadow@live.in>
2015-12-12 01:14:41 +0800

    Alarm Clock

--- threads/thread.h
+++ threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    int block_ticks;                    /* Thread blocked time. */
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
