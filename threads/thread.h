b34e4ecae4fcba320629cf70bad8059b97ae27d1
jaceyen <meadow@live.in>
2015-12-13 12:27:27 +0800

    normalize

--- threads/thread.h
+++ threads/thread.h
@@ -93,7 +93,7 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
-    int block_ticks;                    /* Thread blocked time. */
+    int64_t block_ticks;                    /* Thread blocked time. */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
