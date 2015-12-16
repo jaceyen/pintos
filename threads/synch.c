e6a3e8206e29a46bce7964bb8ed954ef7471d440
jaceyen <meadow@live.in>
2015-12-16 23:58:46 +0800

    remove modify

--- threads/synch.c
+++ threads/synch.c
@@ -115,10 +115,8 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters))
-  {
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
-  }
   sema->value++;
   thread_yield ();
   intr_set_level (old_level);
